62394be3c4ce75c94d5301e8ea463c231244768d
suituu <suituu@outlook.com>
2026-07-11 08:59:04 +0800

    feat: improve ota logs ui

--- src/views/OtaLogs.vue
+++ src/views/OtaLogs.vue
@@ -17,18 +17,17 @@
         style="width:100%;margin-top:20px;"
     >
 
-        <el-table-column
-            prop="device_id"
-            label="Device"
-        />
+<el-table-column
+    prop="device_id"
+    label="Device"
+    min-width="180"
+/>
 
-
-        <el-table-column
-            prop="firmware_id"
-            label="Firmware ID"
-            width="120"
-        />
-
+<el-table-column
+    prop="firmware_version"
+    label="Version"
+    width="120"
+/>
 
         <el-table-column
             prop="from_version"
@@ -42,22 +41,40 @@
         />
 
 
-        <el-table-column
-            prop="status"
-            label="Status"
-        />
+<el-table-column
+    label="Status"
+    width="140"
+>
+    <template #default="scope">
 
+        <el-tag
+            type="success"
+            v-if="scope.row.status === 'downloaded'"
+        >
+            Downloaded
+        </el-tag>
 
-        <el-table-column
-            prop="ip_address"
-            label="IP"
-        />
+        <el-tag
+            type="info"
+            v-else
+        >
+            {{ scope.row.status }}
+        </el-tag>
 
+    </template>
+</el-table-column>
 
-        <el-table-column
-            prop="created_at"
-            label="Time"
-        />
+<el-table-column
+    prop="ip_address"
+    label="IP"
+    width="150"
+/>
+
+<el-table-column
+    prop="created_at"
+    label="Time"
+    min-width="180"
+/>
 
     </el-table>
 
